d7e46cc884e5b0a319caf491c8203c104f17dea7
Daniel Wilkie <dan@danielwilkie.com>
2016-11-25 17:07:33 +0000

    Still trying to get kube-controller-manager to work on master. No luck yet.

--- kubernetes_master_setup.sh
+++ kubernetes_master_setup.sh
@@ -7,37 +7,44 @@ echo "kubernetes_master_setup.sh"
 
 SYNCEDALLVMS=$1
 
+OPTBIN="/opt/bin"
+echo "adding $OPTBIN to PATH in /etc/profile (ie for all users, permanently)"
+echo -e "\nPATH=$OPTBIN:\$PATH" >> /etc/profile
+
 echo "ETCD: copying extracted binaries to /opt/bin"
-mkdir -p /opt/bin
+mkdir -p $OPTBIN
 cp $SYNCEDALLVMS/etcd-v2.0.0-linux-amd64/etcd* /opt/bin
 
 SERVER_BIN=$SYNCEDALLVMS/kubernetes/server/kubernetes/server/bin
 UBUNTU_MAS=$SYNCEDALLVMS/kubernetes/cluster/ubuntu/master
 
 echo "Kubernetes: copying server/bin files to /opt/bin"
-cp $SERVER_BIN/kube-apiserver /opt/bin
-cp $SERVER_BIN/kube-controller-manager /opt/bin
-cp $SERVER_BIN/kube-scheduler /opt/bin
-# cp $SERVER_BIN/kucecfg /opt/bin
-cp $SERVER_BIN/kubectl /opt/bin
-# cp $SERVER_BIN/kubernetes /opt/bin
+mkdir -p $OPTBIN
+cp $SERVER_BIN/kube-apiserver $OPTBIN
+cp $SERVER_BIN/kube-controller-manager $OPTBIN
+cp $SERVER_BIN/kube-scheduler $OPTBIN
+# cp $SERVER_BIN/kucecfg $OPTBIN
+cp $SERVER_BIN/kubectl $OPTBIN
+# cp $SERVER_BIN/kubernetes $OPTBIN
 
 echo "Kubernetes: copying init_conf files to /etc/init"
+mkdir -p /etc/init
 cp $UBUNTU_MAS/init_conf/etcd.conf /etc/init
 cp $UBUNTU_MAS/init_conf/kube-apiserver.conf /etc/init
 cp $UBUNTU_MAS/init_conf/kube-controller-manager.conf /etc/init
 cp $UBUNTU_MAS/init_conf/kube-scheduler.conf /etc/init
 
 echo "Kubernetes: copying init_scripts files to /etc/init.d"
+mkdir -p /etc/init.d
 cp $UBUNTU_MAS/init_scripts/etcd /etc/init.d
 cp $UBUNTU_MAS/init_scripts/kube-apiserver /etc/init.d
 cp $UBUNTU_MAS/init_scripts/kube-controller-manager /etc/init.d
 cp $UBUNTU_MAS/init_scripts/kube-scheduler /etc/init.d
 
-## NB: there is no default scripts directory !!!
-# echo "Kubernetes: copying default_scripts files to /etc/default"
-# cp $UBUNTU_MAS/default_scripts/etcd /etc/default
-# cp $UBUNTU_MAS/default_scripts/kube-apiserver /etc/default
-# cp $UBUNTU_MAS/default_scripts/kube-controller-manager /etc/default
-# cp $UBUNTU_MAS/default_scripts/kube-scheduler /etc/default
-
+## NB: the default scripts refered to below are not provided - I wrote them (so don't lose them!)
+echo "Kubernetes: copying default_scripts to /etc/default"
+mkdir -p /etc/default
+cp $SYNCEDALLVMS/templates/master/default/etcd /etc/default
+cp $SYNCEDALLVMS/templates/master/default/kube-apiserver /etc/default
+cp $SYNCEDALLVMS/templates/master/default/kube-controller-manager /etc/default
+cp $SYNCEDALLVMS/templates/master/default/kube-scheduler /etc/default
--- kubernetes_node_setup.sh
+++ kubernetes_node_setup.sh
@@ -7,25 +7,33 @@ echo "kubernetes_node_setup.sh"
 
 SYNCEDALLVMS=$1
 
+OPTBIN="/opt/bin"
+echo "adding $OPTBIN to PATH in /etc/profile (ie for all users, permanently)"
+echo -e "\nPATH=$OPTBIN:\$PATH" >> /etc/profile
+
 SERVER_BIN=$SYNCEDALLVMS/kubernetes/server/kubernetes/server/bin
 UBUNTU_NOD=$SYNCEDALLVMS/kubernetes/cluster/ubuntu/minion
 
 echo "Kubernetes: copying server/bin files to /opt/bin"
-cp $SERVER_BIN/kubelet /opt/bin
-cp $SERVER_BIN/kube-proxy /opt/bin
-# cp $SERVER_BIN/kucecfg /opt/bin
-cp $SERVER_BIN/kubectl /opt/bin
-# cp $SERVER_BIN/kubernetes /opt/bin
+mkdir -p $OPTBIN
+cp $SERVER_BIN/kubelet $OPTBIN
+cp $SERVER_BIN/kube-proxy $OPTBIN
+# cp $SERVER_BIN/kucecfg $OPTBIN
+cp $SERVER_BIN/kubectl $OPTBIN
+# cp $SERVER_BIN/kubernetes $OPTBIN
 
 echo "Kubernetes: copying init_conf files to /etc/init"
+mkdir -p /etc/init
 cp $UBUNTU_NOD/init_conf/kubelet.conf /etc/init
 cp $UBUNTU_NOD/init_conf/kube-proxy.conf /etc/init
 
 echo "Kubernetes: copying init_scripts files to /etc/init.d"
+mkdir -p /etc/init.d
 cp $UBUNTU_NOD/init_scripts/kubelet /etc/init.d
 cp $UBUNTU_NOD/init_scripts/kube-proxy /etc/init.d
 
-## NB: there is no default scripts directory !!!
-# echo "Kubernetes: copying default_scripts files to /etc/default"
-# cp $UBUNTU_NOD/default_scripts/kubelet /etc/default
-# cp $UBUNTU_NOD/default_scripts/kube-proxy /etc/default
+## NB: the default scripts refered to below are not provided - I wrote them (so don't lose them!)
+echo "Kubernetes: copying default_scripts to /etc/default"
+mkdir -p /etc/default
+cp $SYNCEDALLVMS/templates/node/default/kubelet /etc/default
+cp $SYNCEDALLVMS/templates/node/default/kube-proxy /etc/default
